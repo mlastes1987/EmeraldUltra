5157690f366c64bf3d34bce566c974ae57b21d0f
MARK\mlast <mlastes1987@gmail.com>
2025-05-17 16:08:56 -0400

    Merge with RH-Hideout upstream, re-enabled features.

--- src/data/items.h
+++ src/data/items.h
@@ -13560,7 +13560,7 @@ const struct Item gItemsInfo[] =
     [ITEM_LINKING_CORD] =
     {
         .name = _("Linking Cord"),
-        .price = 8000,
+        .price = 1500,
         .description = COMPOUND_STRING(
             "A mysterious string\n"
             "that makes some\n"
