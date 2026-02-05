28b31e7853a489c46a565d6eb829efe364b7bd42
MARK\mlast <mlastes1987@gmail.com>
2026-02-05 15:51:44 -0500

    Change makefile for filename, change text for Rustboro gym for reusable TMs, add Linking Cord to Lilycove City Dept. Store, making it 1500 pokedollars and changed movesets for Kanto starters and hack starters.

--- src/data/items.h
+++ src/data/items.h
@@ -14918,7 +14918,7 @@ const struct Item gItemsInfo[] =
     [ITEM_LINKING_CORD] =
     {
         .name = ITEM_NAME("Linking Cord"),
-        .price = 8000,
+        .price = 1500,
         .description = COMPOUND_STRING(
             "A mysterious string\n"
             "that makes some\n"
